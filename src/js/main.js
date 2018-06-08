b4d40d1e4dd912d931da733282050e8b4c7e5e0f
Valery Alikin <weblcw@gmail.com>
2018-06-08 22:18:49 +0500

    v0.2

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,268 @@
+var readyTrigger = false, // определяет оконачание анимации, если false - нельзя менять слайды
+    scrollTrigger = true, // событие скролл запускает функцию один раз
+    windowHeight = $(window).height(),
+    windowWidth = $(window).width(),
+    state = 0, // номер слайда на экране
+    slideCount = $('.slide').length - 1; // количество слайдов
+
+var purple = '#544594',
+    green = '#019386';
+
 $(document).ready(function () {
 
 });
 
-anime({targets: '.backdrop',translateX: '-100%',duration: 0});
-anime({targets: '.backdrop-second',translateX: '-100%',duration: 0});
-anime({targets: '.logo',translateX: 123,duration: 0});
-anime({targets: '.logo__text',translateX: 100,opacity: 0,duration: 0});
+// установка состояний элементов по умолчанию
+anime({
+    targets: '.backdrop',
+    translateX: '-100%',
+    duration: 0
+});
+anime({
+    targets: '.backdrop-two',
+    translateX: '-100%',
+    duration: 0
+});
+anime({
+    targets: '.backdrop-three',
+    translateX: '100%',
+    duration: 0
+});
+anime({
+    targets: '.logo',
+    translateX: 123,
+    duration: 0
+});
+anime({
+    targets: '.logo__text',
+    translateX: 100,
+    opacity: 0,
+    duration: 0
+});
 
-function slideTwo() {
+// переход с первого слайда на нулевой
+function gotoSlideZero() {
     var tl = anime.timeline();
     tl.add({
-        targets: '.backdrop-second',
-        translateX: '50%',
+            targets: '.backdrop-two',
+            translateX: '-100%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+        })
+        .add({
+            targets: '.backdrop',
+            translateY: '0',
+            duration: 1200,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+        })
+        .add({
+            targets: '.logo',
+            translateY: 0,
+            duration: 1500,
+            offset: '-=1200',
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+            complete: function () {
+                state = 0;
+                scrollTrigger = true;
+                readyTrigger = true;
+                $('.down').fadeIn();
+                console.log('state : ' + state);
+                console.log('SHOW SLIDE #' + state);
+            }
+        })
+    setTimeout(function () {
+        $('.slide-1').removeClass('active');
+    }, 800);
+}
+
+// переход с нулевого слайда на первый
+function gotoFirstSlide() {
+    $('.down').fadeOut();
+    anime({
+        targets: '.backdrop',
+        translateY: '-100%',
         duration: 1500,
         easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+        complete: function () {
+            nextSlide();
+        }
     })
-    setTimeout(function() {
-        $('.slide-2').addClass('active');
-    },700);
+    anime({
+        targets: '.logo',
+        translateY: windowHeight * -1,
+        duration: 1200,
+        easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+    })
+}
+
+// переключение на следующий слайд
+function nextSlide() {
+    if (state == 0) { // переход с нулевого слайда на первый
+        state++;
+
+        console.log('state : ' + state);
+        console.log('SHOW SLIDE #' + state);
+
+        anime({
+            targets: '.backdrop-two',
+            translateX: '50%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+            complete: function () {
+                scrollTrigger = true;
+                readyTrigger = true;
+            }
+        })
+        setTimeout(function () {
+            $('.slide-' + state).addClass('active');
+        }, 650);
+    } else if (state % 2 == 1) { // переход на четный слайд
+        state++;
+
+        console.log('state : ' + state);
+        console.log('SHOW SLIDE #' + state);
+
+        if (state == 4) {
+            $('.backdrop-three').css('background', green);
+        }
+
+        anime({
+            targets: '.backdrop-two',
+            translateX: '-100%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+        })
+        setTimeout(function () {
+            $('.slide').removeClass('active');
+        }, 750);
+
+        anime({
+            targets: '.backdrop-three',
+            translateX: '-50%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+            delay: 800,
+            complete: function () {
+                scrollTrigger = true;
+                readyTrigger = true;
+            }
+        })
+        setTimeout(function () {
+            $('.slide-' + state).addClass('active');
+        }, 1450);
+    } else if (state % 2 != 1) { // преход на нечетный слайд
+        state++;
+
+        console.log('state : ' + state);
+        console.log('SHOW SLIDE #' + state);
+
+        if (state == 3) {
+            $('.backdrop-two').css('background', green);
+        }
+
+        anime({
+            targets: '.backdrop-three',
+            translateX: '100%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+        })
+        setTimeout(function () {
+            $('.slide').removeClass('active');
+        }, 750);
+
+        anime({
+            targets: '.backdrop-two',
+            translateX: '50%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+            delay: 800,
+            complete: function () {
+                scrollTrigger = true;
+                readyTrigger = true;
+            }
+        })
+        setTimeout(function () {
+            $('.slide-' + state).addClass('active');
+        }, 1450);
+    }
+}
+
+// переключение на предыдущий слайд
+function prevSlide() {
+    if (state % 2 == 1) { // переход на четный слайд        
+        state--;
+
+        if (state == 2) {
+            $('.backdrop-three').css('background', purple);
+        }
+
+        console.log('state : ' + state);
+        console.log('SHOW SLIDE #' + state);
+
+        anime({
+            targets: '.backdrop-two',
+            translateX: '-100%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+        })
+        setTimeout(function () {
+            $('.slide').removeClass('active');
+        }, 750);
+
+        anime({
+            targets: '.backdrop-three',
+            translateX: '-50%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+            delay: 800,
+            complete: function () {
+                scrollTrigger = true;
+                readyTrigger = true;
+            }
+        })
+        setTimeout(function () {
+            $('.slide-' + state).addClass('active');
+        }, 1450);
+    } else if (state % 2 != 1) { // преход на нечетный слайд
+        state--;
+        
+        if (state == 1) {
+            $('.backdrop-two').css('background', purple);
+        }
+
+        console.log('state : ' + state);
+        console.log('SHOW SLIDE #' + state);
+
+        anime({
+            targets: '.backdrop-three',
+            translateX: '100%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+        })
+        setTimeout(function () {
+            $('.slide').removeClass('active');
+        }, 750);
+
+        anime({
+            targets: '.backdrop-two',
+            translateX: '50%',
+            duration: 1500,
+            easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
+            delay: 800,
+            complete: function () {
+                scrollTrigger = true;
+                readyTrigger = true;
+            }
+        })
+        setTimeout(function () {
+            $('.slide-' + state).addClass('active');
+        }, 1450);
+    }
 }
 
 $(window).load(function () {
-    var downTrigger = false,
-        windowHeight = $(window).height(),
-        windowWidth = $(window).width();
+    console.log('state : ' + state);
+    console.log('SHOW SLIDE #' + state);
 
     var tl = anime.timeline();
     tl.add({
@@ -53,7 +292,7 @@ $(window).load(function () {
                     duration: 1000,
                     easing: 'easeInOutCubic',
                     direction: 'alternate',
-                    loop: true,                    
+                    loop: true,
                 })
             }
         })
@@ -64,28 +303,44 @@ $(window).load(function () {
             easing: 'easeInOutCubic',
             offset: '-=500',
             begin: function () {
-                downTrigger = true;
+                readyTrigger = true;
+                scrollTrigger = true;
             }
         })
 
-        $('.down').click(function() {
-            if(downTrigger) {
-                $(this).fadeOut();
-                anime({
-                    targets: '.backdrop',
-                    translateY: '-100%',
-                    duration: 1500,
-                    easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
-                    complete: function() {
-                        slideTwo();
-                    }
-                })
-                anime({
-                    targets: '.logo',
-                    translateY: windowHeight * -1,
-                    duration: 1200,
-                    easing: 'easeInOutCubic', //easeInOutCubic - 1500, easeInOutQuart -2000
-                })
-            }
-        });
+    $('.down').click(function () {
+        console.log("readyTrigger: " + readyTrigger);
+        console.log('scrollTrigger: ' + scrollTrigger);
+        if (readyTrigger) {
+            scrollTrigger = false;
+            readyTrigger = false;
+            gotoFirstSlide();
+        }
+    });
+});
+
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+    console.log("readyTrigger: " + readyTrigger);
+    console.log('scrollTrigger: ' + scrollTrigger);
+    if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+        if (readyTrigger && scrollTrigger && state == 1) {
+            scrollTrigger = false;
+            readyTrigger = false;
+            gotoSlideZero();
+        } else if (readyTrigger && scrollTrigger && state > 0) {
+            scrollTrigger = false;
+            readyTrigger = false;
+            prevSlide();
+        }
+    } else {
+        if (readyTrigger && scrollTrigger && state == 0) {
+            scrollTrigger = false;
+            readyTrigger = false;
+            gotoFirstSlide();
+        } else if (readyTrigger && scrollTrigger && state < slideCount) {
+            scrollTrigger = false;
+            readyTrigger = false;
+            nextSlide();
+        }
+    }
 });
